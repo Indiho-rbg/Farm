198f8c9bfca0188ad2c0ac7dd741abd9a0e4dda6
Indiho-rbg <sonoky1995@gmail.com>
2025-02-13 21:20:50 +0200

    Update node.js

--- node.js
+++ node.js
@@ -1,13 +1,13 @@
 const { Telegraf } = require('telegraf');
 
-const bot = new Telegraf('YOUR_BOT_TOKEN');
+const bot = new Telegraf('7578633286:AAESGkDK5YdSLqlobv0ut1W3ozVRKJtghNE');
 
 bot.start((ctx) => {
     console.log("Отримано /start від", ctx.from.username); // Лог для перевірки
     ctx.reply('Привіт! Натисни кнопку, щоб грати:', {
         reply_markup: {
             inline_keyboard: [
-                [{ text: 'Грати', web_app: { url: 'https://your-vercel-game.vercel.app' } }]
+                [{ text: 'Грати', web_app: { url: 'https://vercel.com/indihos-projects/farm' } }]
             ]
         }
     });
