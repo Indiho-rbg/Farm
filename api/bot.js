c276a4afb55e1206c96c17924805f09f4db2b44d
Indiho-rbg <sonoky1995@gmail.com>
2025-02-13 21:34:58 +0200

    Update bot.js

--- api/bot.js
+++ api/bot.js
@@ -2,6 +2,8 @@ const { Telegraf } = require('telegraf');
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
+bot.launch(); // Запускаємо бота
+
 module.exports = async (req, res) => {
     if (req.method === 'POST') {
         await bot.handleUpdate(req.body);
