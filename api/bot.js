88617cbb6b33117df4534084627ba277bec48f9e
Indiho-rbg <sonoky1995@gmail.com>
2025-02-13 21:24:44 +0200

    Create bot.js

---
+++ api/bot.js
@@ -0,0 +1,10 @@
+const { Telegraf } = require('telegraf');
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+module.exports = async (req, res) => {
+    if (req.method === 'POST') {
+        await bot.handleUpdate(req.body);
+    }
+    res.status(200).send('OK');
+};
