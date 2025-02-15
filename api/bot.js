b8c95bc0ae2f7fd3a770bd87880f61e752872c75
Indiho-rbg <sonoky1995@gmail.com>
2025-02-15 17:18:39 +0200

    Update bot.js

--- api/bot.js
+++ api/bot.js
@@ -2,12 +2,18 @@ const { Telegraf } = require('telegraf');
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
+bot.start((ctx) => {
+    console.log("Отримано команду /start від:", ctx.from.id);
+    ctx.reply("Привіт! Це тестовий бот.");
+});
+
 module.exports = async (req, res) => {
     if (req.method === 'POST') {
+        console.log("Отримано новий запит:", req.body);
         await bot.handleUpdate(req.body);
         return res.status(200).send('OK');
     }
 
-    // Якщо запит не POST – повертаємо 405 (Method Not Allowed)
+    console.log("Отримано некоректний запит:", req.method);
     res.status(405).send('Method Not Allowed');
 };
