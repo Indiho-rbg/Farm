54c5de6a9220b2cdffb4d0c699d62e70a9870e8a
Indiho-rbg <sonoky1995@gmail.com>
2025-02-15 17:15:16 +0200

    Update bot.js

--- api/bot.js
+++ api/bot.js
@@ -2,11 +2,12 @@ const { Telegraf } = require('telegraf');
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-bot.launch(); // Запускаємо бота
-
 module.exports = async (req, res) => {
     if (req.method === 'POST') {
         await bot.handleUpdate(req.body);
+        return res.status(200).send('OK');
     }
-    res.status(200).send('OK');
+
+    // Якщо запит не POST – повертаємо 405 (Method Not Allowed)
+    res.status(405).send('Method Not Allowed');
 };
